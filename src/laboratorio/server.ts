3f7b804896943146d68e9cc4ba0e0561b2e2228c
alu0101410869 <alu0101410869@ull.edu.es>
2023-04-20 09:21:24 +0000

    ejercicio laboratorio

---
+++ src/laboratorio/server.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import { exec } from 'child_process';
+
+const server = express();
+
+/**
+ * Ruta execmd para ejecutar un comando Unix/Linux.
+ */
+server.get('/execmd', (req, res) => {
+    if(!req.query.cmd) {
+        return res.status(400).end();
+    }
+
+    let args = req.query.arg;
+
+    if(args === undefined) {
+        args = '';
+    }
+
+    exec(`${req.query.cmd} ${args}`, (error, stdout, stderr) => {
+        if(error) {
+            return res.status(500).send(JSON.stringify({'type': 'error', 'output': stderr}) + '\n');
+        } else {
+            return res.send(JSON.stringify({'type': 'error', 'output': stdout}) + '\n');
+        }
+    });
+
+    return;
+});
+
+/**
+ * Ruta no válida.
+ */
+server.get('', (_, res) => {
+    res.status(404).end();
+});
+
+server.listen(3000, () => {
+    console.log('Server is up on port 3000');
+})
